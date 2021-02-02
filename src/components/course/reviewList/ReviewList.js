79cb38c59210874e61a862832a288f4300a71a5e
Tomasz Karpeta <b3niek@gmail.com>
2021-02-02 16:31:49 +0100

    Added validation of reviews amount and loading

--- src/components/course/reviewList/ReviewList.js
+++ src/components/course/reviewList/ReviewList.js
@@ -7,6 +7,9 @@ import { Typography } from '@material-ui/core';
 
 const ReviewList = ({ id }) => {
   const [reviews, setReviews] = useState([]);
+  const [loading, setLoading] = useState(
+    'Loading reviews...'
+  );
 
   useEffect(() => {
     const fetchReviews = async () => {
@@ -16,6 +19,11 @@ const ReviewList = ({ id }) => {
         );
         console.log(data.result);
         setReviews(data.result);
+        if (data.result.length > 1) {
+          setLoading(false);
+        } else {
+          setLoading('No reviews yet!');
+        }
       } catch (err) {
         console.error(err);
       }
@@ -26,34 +34,44 @@ const ReviewList = ({ id }) => {
 
   return (
     <Container>
-      <Typography variant="h4">REVIEWS:</Typography>
-      <br />
-      <br />
-      {reviews.map((val, index) => {
-        return (
-          <Container
-            className={styles.container}
-            maxWidth="md"
-            key={index}
-          >
-            <div className={styles.nameCont}>
-              <div>
-                <Typography variant="h6">
-                  {val.author}
+      {loading ? (
+        <Typography align="center" variant="h6">
+          {loading}
+        </Typography>
+      ) : (
+        <React.Fragment>
+          <Typography variant="h4">REVIEWS:</Typography>
+          <br />
+          <br />
+          {reviews.map((val, index) => {
+            return (
+              <Container
+                className={styles.container}
+                maxWidth="md"
+                key={index}
+              >
+                <div className={styles.nameCont}>
+                  <div>
+                    <Typography variant="h6">
+                      {val.author}
+                    </Typography>
+                    <Typography>{val.date}</Typography>
+                  </div>
+                  <Recommend recommend={val.recommend} />
+                </div>
+
+                <br />
+                <hr />
+                <br />
+
+                <Typography paragraph>
+                  {val.review}
                 </Typography>
-                <Typography>{val.date}</Typography>
-              </div>
-              <Recommend recommend={val.recommend} />
-            </div>
-
-            <br />
-            <hr />
-            <br />
-
-            <Typography paragraph>{val.review}</Typography>
-          </Container>
-        );
-      })}
+              </Container>
+            );
+          })}
+        </React.Fragment>
+      )}
     </Container>
   );
 };
