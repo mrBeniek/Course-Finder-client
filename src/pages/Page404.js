a0f43dc9b2c93f696d7be08706bd07be66191abd
Tomasz Karpeta <b3niek@gmail.com>
2021-02-04 22:23:47 +0100

    Minor styling

--- src/pages/Page404.js
+++ src/pages/Page404.js
@@ -1,3 +1,4 @@
+import styles from './Page404.module.scss';
 import React from 'react';
 import Layout from 'components/layout/Layout';
 import Typography from '@material-ui/core/Typography';
@@ -6,7 +7,7 @@ const Page404 = () => {
   return (
     <div>
       <Layout title="Page Not Found">
-        <div className="center-page white-font">
+        <div className={styles.text}>
           <Typography variant="h6">
             Oops! Seems you got lost time traveler, because
             this page doesn't exist, yet...
