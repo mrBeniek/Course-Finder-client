53f63a8da30e4260b394f40cb2f4713b395625f7
Tomasz Karpeta <b3niek@gmail.com>
2021-05-11 18:18:32 +0200

    Refactored Verify page

--- src/pages/Verify.js
+++ src/pages/Verify.js
@@ -1,25 +1,12 @@
-import styles from './Verify.module.scss';
-import React, { useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+import React from 'react';
 import { useParams } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import Layout from 'components/layout/Layout';
-import Typography from '@material-ui/core/Typography';
+import Info from 'components/common/Info';
 
 const Verify = () => {
   const history = useHistory();
   const { type } = useParams();
-
-  useEffect(() => {
-    if (type === 'success' || type === 'error') {
-      setTimeout(() => {
-        type === 'success'
-          ? history.push('/login')
-          : history.push('/');
-      }, 5000);
-    } else if (type !== 'pending') {
-      history.push('/404');
-    }
-  });
 
   const TITLE =
     type === 'success'
@@ -28,19 +15,30 @@ const Verify = () => {
       ? 'Account created successfuly'
       : 'Email verification failed';
 
+  const MSG =
+    type === 'success'
+      ? 'Your email address has been verified successfuly. You can login now'
+      : type === 'pending'
+      ? `Your account has been created successfuly.
+               Course Finder requires email verification. Head to the email address you provided and click the link to activate your account`
+      : "Something went wrong with email verification. Probably you're already verified or the code is invalid";
+
+  const REDIRECT = () => {
+    if (type === 'success' || type === 'error') {
+      if (type === 'success') {
+        return ['/login', 5000];
+      } else return ['/', 5000];
+    } else if (type !== 'pending') history.replace('/404');
+  };
+
   return (
     <div>
       <Layout title={TITLE}>
-        <div className={styles.text}>
-          <Typography variant="h6">
-            {type === 'success'
-              ? 'Your email address has been verified successfuly. You can login now'
-              : type === 'pending'
-              ? `Your account has been created successfuly.
-               Course Finder requires email verification. Head to the email address you provided and click the link to activate your account`
-              : "Something went wrong with email verification. Probably you're already verified or the code is invalid"}
-          </Typography>
-        </div>
+        <Info
+          status={type}
+          msg={MSG}
+          redirect={REDIRECT()}
+        />
       </Layout>
     </div>
   );
