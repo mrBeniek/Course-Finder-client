36afb2dc32b6b0cb063dd3d7e3ebc32b52472bcb
Tomasz Karpeta <b3niek@gmail.com>
2021-08-14 14:30:39 +0200

    Github naming fix setup

--- src/pages/AuthGitHubInfo.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import GitHubInfo from 'components/auth/GitHubInfo';
-
-const AuthGitHubInfoPage = () => {
-  return <GitHubInfo />;
-};
-
-export default AuthGitHubInfoPage;
